a0c22e34184f5264e54b987a73b34374fcbe55c9
Martijn Kunstman <martijn@kunstman.nl>
2024-07-10 21:40:12 +0200

    init

---
+++ src/ComposerPackageExample.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Martijnkunstman\ComposerPackageExample;
+
+class ComposerPackageExample
+{
+    public function greet($greet = "Hello World")
+    {
+        return $greet;
+    }
+}
